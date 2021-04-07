dd13b3918230a551191d4fa37502d0c988322ea4
Briand <briand.thibaud99@gmail.com>
2021-04-07 11:45:28 +0200

    fix attackEnnemie can't be <0

--- src/model/Pokemon.model.ts
+++ src/model/Pokemon.model.ts
@@ -1,12 +1,57 @@
 interface IPokemon{
     name: string;
+    hp: number;
+    attack: number;
+    defense: number;
+    speed: number;
+
 }
 
 export class PokemonModel implements IPokemon{
     private _name: string;
+    private _attack: number;
+    private _defense: number;
+    private _hp: number;
+    private _speed: number;
 
-    constructor(name: string) {
-        this._name = name;
+    constructor(props: IPokemon) {
+        this._name = props.name;
+        this._attack = props.attack;
+        this._defense = props.defense;
+        this._hp = props.hp;
+        this._speed = props.speed;
+    }
+
+    get attack(): number {
+        return this._attack;
+    }
+
+    set attack(value: number) {
+        this._attack = value;
+    }
+
+    get defense(): number {
+        return this._defense;
+    }
+
+    set defense(value: number) {
+        this._defense = value;
+    }
+
+    get hp(): number {
+        return this._hp;
+    }
+
+    set hp(value: number) {
+        this._hp = value;
+    }
+
+    get speed(): number {
+        return this._speed;
+    }
+
+    set speed(value: number) {
+        this._speed = value;
     }
 
     get name(): string {
@@ -15,6 +60,15 @@ export class PokemonModel implements IPokemon{
 
     set name(value: string) {
         this._name = value;
+    }
+
+    attackEnnemie(power: number, pokemon_2: PokemonModel){
+        if (pokemon_2.hp > power){
+            pokemon_2.hp = pokemon_2.hp - power;
+        }else{
+            pokemon_2.hp = 0;
+        }
+
     }
 }
 
